24df189851b27bf09a923bb9094bcd80e39c9a26
piyushsborse <piyushsborse25@gmail.com>
2025-01-05 00:01:55 +0530

    Sve working fine

--- src/app/json-bill-editor/json-bill-editor.component.ts
+++ src/app/json-bill-editor/json-bill-editor.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
 import { BillService } from '../services/bill.service';
 import { Bill } from '../entities/bill';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-json-bill-editor',
@@ -18,14 +19,10 @@ export class JsonBillEditorComponent implements OnInit {
 
   constructor(public billService: BillService) {}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   updateJsonData(): void {
     try {
-      console.log(this.jsonData);
-      
       if (this.jsonString !== undefined) {
         this.jsonData = JSON.parse(this.jsonString);
         this.jsonString = JSON.stringify(this.jsonData, null, 2);
@@ -34,6 +31,11 @@ export class JsonBillEditorComponent implements OnInit {
   }
 
   save() {
-    this.billService.save(this.jsonData);
+    this.billService
+      .save(this.jsonData)
+      .subscribe((result: Bill) => {
+        this.jsonData = result;
+        this.jsonString = JSON.stringify(this.jsonData, null, 2);
+      });
   }
 }
--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -1,17 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Bill } from '../entities/bill';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BillService {
-
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {}
 
   public save(bill: Bill) {
-    this.http.post("http://localhost:8086/bill-service/bill/save", bill).subscribe(result => {
-      console.log(result);
-    });
+    return this.http.post('http://localhost:8086/bill-service/bill/save', bill);
   }
 }
